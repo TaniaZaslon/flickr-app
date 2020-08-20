83f0da7b9b76329763fc6deb81d5c9d3ae17b865
Tetiana Zaslon <zaslontania@gmail.com>
2020-08-20 15:03:27 +0200

    Add Request to FlickrAPI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
-import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.css";
+import ImageGalleryWithSearch from "./components/image.gallery.with.search.component";
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <header></header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container">
+            <a className="navbar-brand" href="#">
+              TZaslon
+            </a>
+            <ul className="nav justify-content-center w-100">
+              <li className="nav-item"></li>
+            </ul>
+          </div>
+        </nav>
+        <div className="container">
+          <h1 className="my-4 text-center">Flickr image gallery</h1>
+          <ImageGalleryWithSearch />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
